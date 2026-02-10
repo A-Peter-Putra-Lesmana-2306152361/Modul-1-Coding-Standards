795d2782c8a95b076d9b3968c0bed2b0a3cb8f88
Peter Putra Lesmana <158464449+PeterP535@users.noreply.github.com>
2026-02-10 20:27:27 +0700

    fix: correct product controller logic before edit/delete implementation

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -7,36 +7,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.List;
 
 @Controller
 @RequestMapping("/product")
 public class ProductController {
+
     @Autowired
     private ProductService service;
 
     @GetMapping("/create")
-    public String createProductPage(Model model){
-        model.addAttribute("product", new Product());
-        return "product/create";
-    }
-
-    @PostMapping("/create")
-    public String createProduct(@ModelAttribute Product productM, Model model){
+    public String createProductPage(Model model) {
         Product product = new Product();
         model.addAttribute("product", product);
         return "createProduct";
     }
 
-    @PostMapping ("/list")
-    public String productListPage(Model model){
+    @PostMapping("/create")
+    public String createProductPost(@ModelAttribute Product product, Model model) {
+        service.create(product);
+        return "redirect:list";
+    }
+
+    @GetMapping("/list")
+    public String productListPage(Model model) {
         List<Product> allProducts = service.findAll();
         model.addAttribute("products", allProducts);
         return "productList";
     }
-
-
-
-
 }
